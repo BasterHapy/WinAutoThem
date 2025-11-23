7c1b9358565d5a21cd4eebb1d499309c1c76bbc2
开心-开心急了 <jxz911911@qq.com>
2025-11-23 11:04:10 +0800

    upgrade: return sunrise sunset

--- src/one_day.py
+++ src/one_day.py
@@ -1,15 +1,19 @@
 # 这是一个抽象类，用于获取一天的日出和日落时间以及当前时间。
 
 import time
+from src.sun_rise_set import get_sunrise_sunset
 
 class OneDay(object):
     """一天
 
     :param object: Python 基础类
     """
-    def __init__(self):
-        """构造函数"""
-        pass
+    def __init__(self,latlng: tuple[float,float]):
+        """构造函数
+
+        :param latlng: 经纬度
+        """
+        self.sun_info = get_sunrise_sunset(latlng)
     
     def get_current_time(self) -> float:
         """获取当前时间
@@ -23,11 +27,12 @@ class OneDay(object):
 
         :return: Unix 时间戳
         """
-        pass
+        return self.sun_info[0]
+        
 
     def get_sun_set(self) -> float:
         """获取日落时间
 
         :return: Unix 时间戳
         """
-        pass
+        return self.sun_info[1]
